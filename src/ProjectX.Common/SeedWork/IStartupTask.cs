cddef19b38c73772eef0f1ca86b56bc49d63a35a
andriy <a.zubyk@avidi.tech>
2021-02-03 10:43:41 +0200

    added IStartup interface

---
+++ src/ProjectX.Common/SeedWork/IStartupTask.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectX.Common
+{
+    public interface IStartupTask
+    {
+        Task ExecuteAsync(CancellationToken cancellationToken = default);
+    }
+}
