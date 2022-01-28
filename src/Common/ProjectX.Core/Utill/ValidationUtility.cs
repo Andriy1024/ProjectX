4ec6677974dab3a0c70f46a692e10a300958de06
andrii_zubyk <andrii_zubyk@epam.com>
2022-01-28 09:34:51 +0200

    add validation utility

---
+++ src/Common/ProjectX.Core/Utill/ValidationUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace ProjectX.Core
+{
+    public static class ValidationUtility
+    {
+        public static void ThrowIfInvalid<T>(this T obj)
+        {
+            if (obj is null)
+                throw new ArgumentNullException($"{typeof(T).Name}");
+
+            var ctx = new ValidationContext(obj);
+            var results = new List<ValidationResult>();
+
+            if (!Validator.TryValidateObject(obj, ctx, results, true))
+            {
+                throw new ValidationException($"{typeof(T).Name}: {string.Join(',', results.Select(r => r.ErrorMessage))}");
+            }
+        }
+    }
+}
